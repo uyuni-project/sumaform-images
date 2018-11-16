40f321a6c919fdb34587dd7c261886cdd9f3dde8
Silvio Moioli <smoioli@suse.de>
2018-11-16 11:23:11 +0100

    Ubuntu: remove unnecessary files and packages

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -xe
 
+# remove unnecessary packages
+sudo apt remove -y --purge snapd git perl-modules lxd lxd-client xfsprogs
+apt clean
+rm -rf /var/lib/apt/lists/*
+
+# remove locales
+find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' | xargs rm -r
+
 # mark all free space
 fstrim -av
