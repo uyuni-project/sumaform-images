1e1033321a4140543ef0d7d84d829f5fcd6733e9
Silvio Moioli <moio@suse.com>
2019-10-04 13:31:08 +0200

    Ubuntu: make sure no machine-id stays present
    This leads to the creation of VMs that might be assigned the same IP
    address from libvirtd.

--- scripts/ubuntu-cleanup.sh
+++ scripts/ubuntu-cleanup.sh
@@ -9,5 +9,8 @@ rm -rf /var/lib/apt/lists/*
 # remove locales
 find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' | xargs rm -r
 
+# remove machine IDs
+rm -f /etc/machine-id && rm -f /var/lib/dbus/machine-id && touch /etc/machine-id
+
 # mark all free space
 fstrim -av
